832b794f55a3267af6a13fe616cbcd411cdef8e0
Richard Gieg <richgieg@users.noreply.github.com>
2026-03-07 09:40:07 -0800

    Ensure scrolled to top on navigate

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,22 @@
-import { BrowserRouter } from 'react-router-dom'
+import { useEffect } from 'react'
+import { BrowserRouter, useLocation } from 'react-router-dom'
 
 import { AppRoutes } from './router'
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+  }, [pathname])
+
+  return null
+}
 
 export const App = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <AppRoutes />
     </BrowserRouter>
   )
