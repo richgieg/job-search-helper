70f85716cf06b9f8c20b83ce4a0727b244f46053
Richard Gieg <richgieg@users.noreply.github.com>
2026-03-14 15:29:07 -0700

    Fix landing page test

--- src/app/router.test.tsx
+++ src/app/router.test.tsx
@@ -30,7 +30,7 @@ describe('AppRoutes', () => {
 
     renderAppRoutes('/')
 
-    expect(await screen.findByRole('heading', { name: 'Track your search in one place.' })).toBeInTheDocument()
+    expect(await screen.findByRole('heading', { name: 'Everything for your search in one place.' })).toBeInTheDocument()
     expect(screen.getByRole('link', { name: 'Job Search Helper' })).toHaveAttribute('href', '/')
     expect(screen.getByRole('link', { name: 'Dashboard' })).toHaveAttribute('href', '/dashboard')
   })
